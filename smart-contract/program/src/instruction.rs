98d7ba1004c0c5b183b49b2550f4b9956624bc16
DR497 <47689875+dr497@users.noreply.github.com>
2021-11-26 17:48:42 +0800

    program: add doc string

--- smart-contract/program/src/instruction.rs
+++ smart-contract/program/src/instruction.rs
@@ -12,48 +12,99 @@ pub enum MediaInstruction {
     ///
     /// Accounts expected by this instruction:
     ///
-    /// 1. `[writable]` The central state account
-    /// 2. `[]` The system program account
-    /// 3. `[writable, signer]` The fee payer account
-    /// 4. `[]` The rent sysvar account
+    // | Index | Writable | Signer | Description                |
+    // |-------|----------|--------|----------------------------|
+    // | 0     | ✅        | ❌      | The central state account  |
+    // | 1     | ❌        | ❌      | The system program account |
+    // | 2     | ✅        | ✅      | The fee payer account      |
+    // | 3     | ❌        | ❌      | The rent sysvar account    |
     CreateCentralState(create_central_state::Params),
 
     /// Create stake pool
     ///
     /// Accounts expected by this instruction:
     ///
-    /// 1. `[writable]` The stake pool account
-    /// 2. `[]` The system program account
-    /// 3. `[writable, signer]` The fee payer account
-    /// 4. `[]` The rent sysvar account
+    /// | Index | Writable | Signer | Description                |
+    /// |-------|----------|--------|----------------------------|
+    /// | 0     | ✅        | ❌      | The stake pool account     |
+    /// | 1     | ❌        | ❌      | The system program account |
+    /// | 2     | ✅        | ✅      | The fee payer account      |
+    /// | 3     | ❌        | ❌      | The rent sysvar account    |
     CreateStakePool(create_stake_pool::Params),
 
     /// Create stake account
     ///
     /// Accounts expected by this instruction:
     ///
-    /// 1. `[writable]` The stake account
-    /// 2. `[]` The system program account
-    /// 3. `[writable, signer]` The fee payer account
-    /// 4. `[]` The rent sysvar account
+    /// | Index | Writable | Signer | Description                |
+    /// |-------|----------|--------|----------------------------|
+    /// | 0     | ✅        | ❌      | The stake account          |
+    /// | 1     | ❌        | ❌      | The system program account |
+    /// | 2     | ✅        | ✅      | The fee payer account      |
+    /// | 3     | ❌        | ❌      | The rent sysvar account    |
     CreateStakeAccount(create_stake_account::Params),
 
-    // Stake tokens into a stake pool
+    /// Stake tokens into a stake pool
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    /// | Index | Writable | Signer | Description                   |
+    /// |-------|----------|--------|-------------------------------|
+    /// | 0     | ✅        | ❌      | The stake account             |
+    /// | 1     | ✅        | ❌      | The stake pool account        |
+    /// | 2     | ✅        | ✅      | The stake account owner       |
+    /// | 3     | ✅        | ❌      | The source token account      |
+    /// | 4     | ❌        | ❌      | The SPL token program account |
+    /// | 5     | ✅        | ❌      | The vault token account       |
     Stake(stake::Params),
 
-    // Unstake tokens from a stake pool
+    /// Unstake tokens from a stake pool
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    /// | Index | Writable | Signer | Description                   |
+    /// |-------|----------|--------|-------------------------------|
+    /// | 0     | ✅        | ❌      | The stake account             |
+    /// | 1     | ✅        | ❌      | The stake pool account        |
+    /// | 2     | ✅        | ✅      | The stake account owner       |
+    /// | 3     | ✅        | ❌      | The destination token account |
+    /// | 4     | ❌        | ❌      | The SPL token program account |
+    /// | 5     | ✅        | ❌      | The vault token account       |
     Unstake(unstake::Params),
 
-    // Permissionless crank used to transfer tokens
-    // to stake pools owners
+    /// Permissionless crank used to transfer tokens to stake pools owners
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    /// TODO
     ClaimRewards,
 
-    // Close stake pool
+    /// Close stake pool
+    ///
+    /// Accounts expected by this instruction
+    ///
+    // | Index | Writable | Signer | Description                |
+    /// |-------|----------|--------|----------------------------|
+    /// | 0     | ✅        | ❌      | The stake pool account     |
+    /// | 1     | ❌        | ❌      | The system program account |
+    /// | 2     | ✅        | ✅      | The stake pool owner       |
     CloseStakePool,
 
-    // Close stake account
+    /// Close stake account
+    ///
+    /// Accounts expected by this instruction
+    ///
+    /// | Index | Writable | Signer | Description                |
+    /// |-------|----------|--------|----------------------------|
+    /// | 0     | ✅        | ❌      | The stake account          |
+    /// | 1     | ❌        | ❌      | The system program account |
+    /// | 2     | ✅        | ✅      | The stake pool owner       |
     CloseStakeAccount,
 
-    // Change inflation rate
+    /// Change inflation rate
+    ///
+    /// Accounts expected by this instruction
+    ///
+    /// TODO
     ChangeInflation(change_inflation::Params),
 }
