de5ddce469f77c6c931b7c29d3e1a6304d9803c4
Lcchy <lcchy@mailbox.org>
2022-03-29 00:04:48 +0100

    Revert "Reorgranize the stake pool buffer to save space"
    This reverts commit 44e546f9711e2c3cec09002c539ff3a238172449.

--- smart-contract/program/src/state.rs
+++ smart-contract/program/src/state.rs
@@ -135,19 +135,16 @@ pub struct StakePool<H, B> {
 /// The values are stored in the FP32 format.
 #[derive(Pod, Clone, Copy, Zeroable, Debug)]
 #[repr(C)]
-pub struct Rewards {
-    pub(crate) daily_inflation: u64,
-    /// Stored in the FP32 format, value is always < 1<<32
-    pub(crate) pool_reward: u64,
-    /// Stored in the FP32 format, value is always < 1<<32
-    pub(crate) stakers_reward: u64,
+pub struct RewardsTuple {
+    pub(crate) pool_reward: u128,
+    pub(crate) stakers_reward: u128,
 }
 
 #[allow(missing_docs)]
-pub type StakePoolRef<'a> = StakePool<RefMut<'a, StakePoolHeader>, RefMut<'a, [Rewards]>>;
+pub type StakePoolRef<'a> = StakePool<RefMut<'a, StakePoolHeader>, RefMut<'a, [RewardsTuple]>>;
 
 #[allow(missing_docs)]
-pub type StakePoolHeaped = StakePool<Box<StakePoolHeader>, Box<[Rewards]>>;
+pub type StakePoolHeaped = StakePool<Box<StakePoolHeader>, Box<[RewardsTuple]>>;
 
 #[allow(missing_docs)]
 impl<'a> StakePoolRef<'a> {
@@ -176,7 +173,7 @@ impl StakePoolHeaped {
     pub fn from_buffer(buf: &[u8]) -> Self {
         let (header, balances) = buf.split_at(size_of::<StakePoolHeader>());
         let header = from_bytes::<StakePoolHeader>(header);
-        let balances = cast_slice::<_, Rewards>(balances);
+        let balances = cast_slice::<_, RewardsTuple>(balances);
         Self {
             header: Box::new(*header),
             balances: Box::from(balances),
@@ -185,12 +182,12 @@ impl StakePoolHeaped {
 }
 
 #[allow(missing_docs)]
-impl<H: DerefMut<Target = StakePoolHeader>, B: DerefMut<Target = [Rewards]>> StakePool<H, B> {
+impl<H: DerefMut<Target = StakePoolHeader>, B: DerefMut<Target = [RewardsTuple]>> StakePool<H, B> {
     pub fn push_balances_buff(
         &mut self,
         present_time: i64,
         last_crank_time: i64,
-        rewards: Rewards,
+        rewards: RewardsTuple,
     ) -> Result<(), ProgramError> {
         let nb_days_passed = (present_time
             .checked_sub(last_crank_time)
@@ -201,8 +198,7 @@ impl<H: DerefMut<Target = StakePoolHeader>, B: DerefMut<Target = [Rewards]>> Sta
             self.balances[(((self.header.current_day_idx as u64)
                 .checked_add(i)
                 .ok_or(AccessError::Overflow)?)
-                % STAKE_BUFFER_LEN) as usize] = Rewards {
-                daily_inflation: 0,
+                % STAKE_BUFFER_LEN) as usize] = RewardsTuple {
                 pool_reward: 0,
                 stakers_reward: 0,
             };
