43b954232a3105b3617f00524e47188da3027c30
Pavol Husar <phusar@gmail.com>
2022-07-13 08:27:09 +0200

    Check stake for bond accounts

--- backends/js/src/utils/stake.ts
+++ backends/js/src/utils/stake.ts
@@ -1,4 +1,4 @@
-import { StakeAccount, ACCESS_PROGRAM_ID, StakePool } from "@access-protocol";
+import { StakeAccount, ACCESS_PROGRAM_ID, StakePool, BondAccount, getBondAccounts, getAllActiveBonds, getAllInactiveBonds } from "@access-protocol";
 import { connection } from "./connection";
 import { PublicKey } from "@solana/web3.js";
 
@@ -6,6 +6,7 @@ import { PublicKey } from "@solana/web3.js";
  * Public key of the stake pool (must be defined in the .env file)
  */
 const STAKE_POOL_KEY = new PublicKey(process.env.STAKE_POOL_KEY!);
+const BOND_POOL_KEY = process.env.BOND_POOL_KEY!;
 
 /**
  * Verifies that a user has enough tokens staked
@@ -24,10 +25,17 @@ export const checkStake = async (owner: string) => {
     stakeAccount.stakePool
   );
 
+  const bondAccounts = await getBondAccounts(connection, new PublicKey(owner));
+  let bondTotalStaked = 0;
+  for (const bondAccount of bondAccounts) {
+    const account = BondAccount.deserialize(bondAccount.account.data);
+    bondTotalStaked += account.totalStaked.toNumber();
+  }
+
   const requiredAmount = Math.min(
     stakeAccount.poolMinimumAtCreation.toNumber(),
     stakePool.minimumStakeAmount.toNumber()
   );
 
-  return stakeAccount.stakeAmount.toNumber() > requiredAmount;
+  return stakeAccount.stakeAmount.toNumber() + bondTotalStaked > requiredAmount;
 };
