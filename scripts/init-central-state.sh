19a7e5664c62e7e283453b15fd087dc653ab45c9
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:47:33 -0600

    Update init-central-state.sh

--- scripts/init-central-state.sh
+++ scripts/init-central-state.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-set -x
-set -o allexport -o notify
 
-pwd=$(pwd)
-mint_address=$(cat $pwd/artifacts/mint_address.txt)
-echo "mint_address: $mint_address"
-
-PROGRAM_PUBKEY=$(solana-keygen pubkey $pwd/artifacts/program.json)
-AUTHORITY_KEYPAIR="$pwd/artifacts/authority.json"
-MINT_ADDRESS=$mint_address
-YEARLY_INFLATION_IN_ACS=2000000000
-SOLANA_RPC_PROVIDER_URL=https://api.devnet.solana.com
-
-ts-node init-central-state
