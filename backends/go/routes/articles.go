9a4eb2d11081f80f4bff070860339e5995648a1d
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 01:13:12 -0600

    Update articles.go

--- backends/go/routes/articles.go
+++ backends/go/routes/articles.go
@@ -1,12 +1,1 @@
-package routes
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
-
-// Example of a route protected by JWT
-func HandleArticle(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
-}
