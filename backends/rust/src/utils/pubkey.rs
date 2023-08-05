5bfc552b255fe722a0328fe1dd353e1f78dfd9b6
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:51:43 -0600

    Update pubkey.rs

--- backends/rust/src/utils/pubkey.rs
+++ backends/rust/src/utils/pubkey.rs
@@ -1,6 +1,1 @@
-use {solana_program::pubkey::Pubkey, std::str::FromStr};
 
-/// Verifies the validity of a public key
-pub fn is_valid_pubkey(address: &str) -> bool {
-    Pubkey::from_str(address).is_ok()
-}
