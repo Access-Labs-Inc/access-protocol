41b95ed73b28baad1dfe1a4e2902978ff507bd79
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 01:03:04 -0600

    Update validateRequest.ts

--- backends/js/src/utils/validateRequest.ts
+++ backends/js/src/utils/validateRequest.ts
@@ -1,19 +1,1 @@
-import { Schema } from "ajv";
-import { NextFunction, Response, Request } from "express";
-import { ajv } from "./ajv";
 
-/**
- * Express middle ware used to validate requests' bodies
- * @param schema The Ajv schema used to validate the request's body
- * @returns
- */
-export const validaRequestBody =
-  (schema: Schema) => (req: Request, res: Response, next: NextFunction) => {
-    const validate = ajv.compile(schema);
-    const isValid = validate(req.body);
-
-    if (!isValid && req.method !== "GET") {
-      return res.status(400).json(validate.errors);
-    }
-    next();
-  };
