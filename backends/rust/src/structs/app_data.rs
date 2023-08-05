50206c5851943b4bf8a12c4ef4cc213a1603777d
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:53:44 -0600

    Update app_data.rs

--- backends/rust/src/structs/app_data.rs
+++ backends/rust/src/structs/app_data.rs
@@ -1,14 +1,1 @@
-use crate::utils::settings::REDIS_URL;
-use redis::Client;
 
-pub struct AppData {
-    pub redis_client: Client,
-}
-
-impl AppData {
-    pub fn new() -> AppData {
-        Self {
-            redis_client: Client::open(REDIS_URL.as_str()).unwrap(),
-        }
-    }
-}
