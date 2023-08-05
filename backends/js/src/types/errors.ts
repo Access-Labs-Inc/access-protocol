6fb81753836368ac71d2d04ff997fceeba02fcb4
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 01:06:51 -0600

    Update errors.ts

--- backends/js/src/types/errors.ts
+++ backends/js/src/types/errors.ts
@@ -1,6 +1,1 @@
-export enum ErrorMessage {
-  ErrorGeneratingNonce = "Error: generating nonce",
-  InvalidNonce = "Error: invalid nonce",
-  InvalidStake = "Error: invalid stake",
-  ErrorValidatingNonce = "Error: validating nonce",
-}
+
