55e9f0b8a2ecc9e628798ae812c3c3d5616317f6
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:52:01 -0600

    Update nonce.rs

--- backends/rust/src/utils/nonce.rs
+++ backends/rust/src/utils/nonce.rs
@@ -1,7 +1,1 @@
-use {hex, rand::Rng};
 
-/// Generates a random 32 bytes nonce encoded as hex string
-pub fn generate_nonce() -> String {
-    let random_bytes = rand::thread_rng().gen::<[u8; 32]>();
-    hex::encode(random_bytes)
-}
