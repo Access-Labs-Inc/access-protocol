38fd01e8b106e22594e9e5d45caa620de26b1acf
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:54:00 -0600

    Update api_response.rs

--- backends/rust/src/structs/api_response.rs
+++ backends/rust/src/structs/api_response.rs
@@ -1,16 +1,1 @@
-use serde::Serialize;
 
-#[derive(Serialize)]
-pub struct ApiResponse<T> {
-    success: bool,
-    result: T,
-}
-
-impl<T> ApiResponse<T> {
-    pub fn new(success: bool, data: T) -> ApiResponse<T> {
-        Self {
-            success,
-            result: data,
-        }
-    }
-}
