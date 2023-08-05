2bc58a81e9fd8b79160852b1f49593340b6150a0
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 01:07:09 -0600

    Update apiResponse.ts

--- backends/js/src/types/apiResponse.ts
+++ backends/js/src/types/apiResponse.ts
@@ -1,13 +1,1 @@
-import { ErrorMessage } from "./errors";
-import { NonceResponse, LoginResponse } from "./routes";
 
-type Result = ErrorMessage | NonceResponse | LoginResponse;
-
-export class ApiResponse {
-  success: boolean;
-  result: Result | undefined;
-  constructor(success: boolean = false, result?: Result) {
-    this.result = result;
-    this.success = success;
-  }
-}
