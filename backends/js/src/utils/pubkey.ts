75c8c6a84d99c31ceef2b657595199e03a219772
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 01:04:37 -0600

    Update pubkey.ts

--- backends/js/src/utils/pubkey.ts
+++ backends/js/src/utils/pubkey.ts
@@ -1,15 +1,1 @@
-import { PublicKey } from "@solana/web3.js";
 
-/**
- * Verifies if a base58 encoded string is valid public key
- * @param x Base58 encoded public key to validate
- * @returns
- */
-export const isValidPubkey = (x: string) => {
-  try {
-    new PublicKey(x);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
