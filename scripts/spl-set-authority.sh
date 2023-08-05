e02c2e04e650c3b813792e93f5a809b2b9088f0c
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-08-05 00:46:18 -0600

    Update spl-set-authority.sh

--- scripts/spl-set-authority.sh
+++ scripts/spl-set-authority.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-set -x
-set -o allexport -o notify
 
-pwd=$(pwd)
-mint_address=$(cat $pwd/artifacts/mint_address.txt)
-central_state_pubkey=$(cat $pwd/artifacts/central_state_pubkey.txt)
-
-SPL_AUTHORITY_KEYPAIR="$pwd/artifacts/spl_authority.json"
-SOLANA_RPC_PROVIDER_URL=https://api.devnet.solana.com
-MINT_ADDRESS=$mint_address
-NEW_AUTHORITY_ADDRESS=$central_state_pubkey
-
-ts-node spl-set-authority.ts
