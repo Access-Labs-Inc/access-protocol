72d71687afa89acdb9f1cfd097cca79c570e0cd6
Vladislav Matus <vladislav@matus.pro>
2023-09-05 16:14:25 +0200

    Bond V2 - Adding to an existing bond - test forever

--- smart-contract/program/tests/v2_bonds.rs
+++ smart-contract/program/tests/v2_bonds.rs
@@ -11,21 +11,21 @@ pub mod common;
 async fn signed_claim() {
     // Setup the token + basic accounts
     let mut tr = TestRunner::new(1_000_000).await.unwrap();
-    // Create users
-    let stake_pool_owner = tr.create_ata_account().await.unwrap();
-    let from = tr.create_ata_account().await.unwrap();
-    let to = tr.create_ata_account().await.unwrap();
-    // Mint to staker
-    tr.mint(&from.pubkey(), 100_000).await.unwrap();
-    // Create stake pool
-    tr.create_stake_pool(&stake_pool_owner.pubkey(), 10_000).await.unwrap();
-    // Activate stake pool
-    tr.activate_stake_pool(&stake_pool_owner.pubkey()).await.unwrap();
-
     // ---------------------------------------------------------------------------------------------
     // Unlockable bond
     // ---------------------------------------------------------------------------------------------
     {
+        // Create users
+        let stake_pool_owner = tr.create_ata_account().await.unwrap();
+        let from = tr.create_ata_account().await.unwrap();
+        let to = tr.create_ata_account().await.unwrap();
+        // Mint to staker
+        tr.mint(&from.pubkey(), 100_000).await.unwrap();
+        // Create stake pool
+        tr.create_stake_pool(&stake_pool_owner.pubkey(), 10_000).await.unwrap();
+        // Activate stake pool
+        tr.activate_stake_pool(&stake_pool_owner.pubkey()).await.unwrap();
+
         let current_time = tr.get_current_time().await;
         let unlock_date = current_time + 1000;
         let bond_amount = 20_000;
@@ -87,30 +87,69 @@ async fn signed_claim() {
     // ---------------------------------------------------------------------------------------------
     // Forever bond
     // ---------------------------------------------------------------------------------------------
-    // {
-    //     let bond_amount = 20_000;
-    //     tr.create_bond_v2(
-    //         &from,
-    //         &to.pubkey(),
-    //         &stake_pool_owner.pubkey(),
-    //         bond_amount,
-    //         None,
-    //     ).await.unwrap();
-    //
-    //     let staker_stats = tr.staker_stats(from.pubkey()).await.unwrap();
-    //     assert_eq!(staker_stats.balance, 100_000 - 2 * bond_amount - 2 * bond_amount * FEES / 100);
-    //     let pool_stats = tr.pool_stats(stake_pool_owner.pubkey()).await.unwrap();
-    //     assert_eq!(pool_stats.header.total_staked, 2 * bond_amount);
-    //     assert_eq!(pool_stats.vault, bond_amount); // it got burned so it didn't get to the vault
-    //     let central_state_stats = tr.central_state_stats().await.unwrap();
-    //     assert_eq!(central_state_stats.total_staked, 2 * bond_amount);
-    //     let bond = tr.bond_v2_stats(to.pubkey(), stake_pool_owner.pubkey(), None).await.unwrap();
-    //     assert_eq!(bond.tag, BondAccountV2);
-    //     assert_eq!(bond.unlock_date, None);
-    //     assert_eq!(bond.pool, tr.get_pool_pda(&stake_pool_owner.pubkey()));
-    //     assert_eq!(bond.amount, bond_amount);
-    //     assert_eq!(bond.owner, to.pubkey());
-    //     assert_eq!(bond.last_claimed_offset, 0);
-    //     assert_eq!(bond.pool_minimum_at_creation, 10_000);
-    // }
+    {
+        let previously_staked = 30_000;
+        // Create users
+        let stake_pool_owner = tr.create_ata_account().await.unwrap();
+        let from = tr.create_ata_account().await.unwrap();
+        let to = tr.create_ata_account().await.unwrap();
+        // Mint to staker
+        tr.mint(&from.pubkey(), 100_000).await.unwrap();
+        // Create stake pool
+        tr.create_stake_pool(&stake_pool_owner.pubkey(), 10_000).await.unwrap();
+        // Activate stake pool
+        tr.activate_stake_pool(&stake_pool_owner.pubkey()).await.unwrap();
+
+        // Create bond
+        let bond_amount = 30_000;
+        tr.create_bond_v2(
+            &from,
+            &to.pubkey(),
+            &stake_pool_owner.pubkey(),
+            bond_amount,
+            None,
+        ).await.unwrap();
+
+        let staker_stats = tr.staker_stats(from.pubkey()).await.unwrap();
+        assert_eq!(staker_stats.balance, 100_000 -  bond_amount - bond_amount * FEES / 100);
+        let pool_stats = tr.pool_stats(stake_pool_owner.pubkey()).await.unwrap();
+        assert_eq!(pool_stats.header.total_staked, bond_amount);
+        assert_eq!(pool_stats.vault, 0); // it got burned so it didn't get to the vault
+        let central_state_stats = tr.central_state_stats().await.unwrap();
+        assert_eq!(central_state_stats.total_staked, previously_staked + bond_amount);
+        let bond = tr.bond_v2_stats(to.pubkey(), stake_pool_owner.pubkey(), None).await.unwrap();
+        assert_eq!(bond.tag, BondAccountV2);
+        assert_eq!(bond.unlock_date, None);
+        assert_eq!(bond.pool, tr.get_pool_pda(&stake_pool_owner.pubkey()));
+        assert_eq!(bond.amount, bond_amount);
+        assert_eq!(bond.owner, to.pubkey());
+        assert_eq!(bond.last_claimed_offset, 0);
+        assert_eq!(bond.pool_minimum_at_creation, 10_000);
+
+        // Add to bond
+        let add_amount = 40_000;
+        tr.add_to_bond_v2(
+            &from,
+            &to.pubkey(),
+            &stake_pool_owner.pubkey(),
+            add_amount,
+            None,
+        ).await.unwrap();
+
+        let staker_stats = tr.staker_stats(from.pubkey()).await.unwrap();
+        assert_eq!(staker_stats.balance, 100_000 - (bond_amount + add_amount) - (bond_amount + add_amount) * FEES / 100);
+        let pool_stats = tr.pool_stats(stake_pool_owner.pubkey()).await.unwrap();
+        assert_eq!(pool_stats.header.total_staked, (bond_amount + add_amount));
+        assert_eq!(pool_stats.vault, 0);
+        let central_state_stats = tr.central_state_stats().await.unwrap();
+        assert_eq!(central_state_stats.total_staked, previously_staked + (bond_amount + add_amount));
+        let bond = tr.bond_v2_stats(to.pubkey(), stake_pool_owner.pubkey(), None).await.unwrap();
+        assert_eq!(bond.tag, BondAccountV2);
+        assert_eq!(bond.unlock_date, None);
+        assert_eq!(bond.pool, tr.get_pool_pda(&stake_pool_owner.pubkey()));
+        assert_eq!(bond.amount, (bond_amount + add_amount));
+        assert_eq!(bond.owner, to.pubkey());
+        assert_eq!(bond.last_claimed_offset, 0);
+        assert_eq!(bond.pool_minimum_at_creation, 10_000);
+    }
 }
