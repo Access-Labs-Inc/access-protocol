f25cb6a10d11145f2c61f1b6b4bea159c952e4fe
Vladislav Matus <vladislav@matus.pro>
2023-09-25 14:09:31 +0200

    Full devnet deploy

--- scripts/deploy-full-devnet.sh
+++ scripts/deploy-full-devnet.sh
@@ -11,3 +11,6 @@ echo "STEP 3: Initializing the central state"
 
 echo "STEP 4: Transferring the SPL token authority to the central state"
 ./spl-set-authority.sh
+
+echo "STEP 5: Migrating the central state to the V2 format"
+./migrate-central-state-v2.sh
