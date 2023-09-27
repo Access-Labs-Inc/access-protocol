5ef6445e287f10e08110ba240b9fd48e052c55dc
Vladislav Matus <vladislav@matus.pro>
2023-09-27 11:38:51 +0200

    spl-mint.sh script added

--- scripts/deploy-full-devnet.sh
+++ scripts/deploy-full-devnet.sh
@@ -1,16 +1,26 @@
-mkdir artifacts
+#!/bin/bash
+set -x
+set -o allexport -o notify
+set -e
 
-echo "STEP 1: Creating an SPL Token"
-ts-node create-devnet-spl-token.ts
+#mkdir -p artifacts
+#
+#echo "STEP 1: Creating an SPL Token"
+#ts-node create-devnet-spl-token.ts
+#
+#echo "STEP 2: Deploying the Devnet contract with V1 instructions allowed"
+## the ALLOW_V1 flag is only needed if we want to keep the V1 instructions enabled
+## ALLOW_V1=true ./deploy-contract.sh
+#./deploy-contract.sh
+#
+#echo "STEP 3: Initializing the central state"
+#./init-central-state.sh
 
-echo "STEP 2: Deploying the Devnet contract with V1 instructions allowed"
-ALLOW_V1=true ./deploy-contract.sh
+echo "STEP 4: Minting 1,000,000 tokens to token bank as minting is disabled in V2 (if ALLOW_V1=false)"
+./spl-mint.sh
 
-echo "STEP 3: Initializing the central state"
-./init-central-state.sh
-
-echo "STEP 4: Transferring the SPL token authority to the central state"
+echo "STEP 5: Transferring the SPL token authority to the central state"
 ./spl-set-authority.sh
 
-echo "STEP 5: Migrating the central state to the V2 format"
+echo "STEP 6: Migrating the central state to the V2 format"
 ./migrate-central-state-v2.sh
