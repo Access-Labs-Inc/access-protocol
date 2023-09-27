e11a492482da4bee842188424fbbb762e5d31b17
Vladislav Matus <vladislav@matus.pro>
2023-09-27 12:29:59 +0200

    Full devnet deployment working

--- scripts/deploy-full-devnet.sh
+++ scripts/deploy-full-devnet.sh
@@ -3,18 +3,18 @@ set -x
 set -o allexport -o notify
 set -e
 
-#mkdir -p artifacts
-#
-#echo "STEP 1: Creating an SPL Token"
-#ts-node create-devnet-spl-token.ts
-#
-#echo "STEP 2: Deploying the Devnet contract with V1 instructions allowed"
-## the ALLOW_V1 flag is only needed if we want to keep the V1 instructions enabled
-## ALLOW_V1=true ./deploy-contract.sh
-#./deploy-contract.sh
-#
-#echo "STEP 3: Initializing the central state"
-#./init-central-state.sh
+mkdir -p artifacts
+
+echo "STEP 1: Creating an SPL Token"
+ts-node create-devnet-spl-token.ts
+
+echo "STEP 2: Deploying the Devnet contract with V1 instructions allowed"
+# the ALLOW_V1 flag is only needed if we want to keep the V1 instructions enabled
+# ALLOW_V1=true ./deploy-contract.sh
+./deploy-contract.sh
+
+echo "STEP 3: Initializing the central state"
+./init-central-state.sh
 
 echo "STEP 4: Minting 1,000,000 tokens to token bank as minting is disabled in V2 (if ALLOW_V1=false)"
 ./spl-mint.sh
