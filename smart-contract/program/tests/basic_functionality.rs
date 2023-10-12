e26fe949dd02b43efa3a5693b3bc88b45de8f463
Vladislav Matus <vladislav@matus.pro>
2023-10-12 12:25:54 +0200

    Change freeze authority test

--- smart-contract/program/tests/basic_functionality.rs
+++ smart-contract/program/tests/basic_functionality.rs
@@ -1,5 +1,7 @@
-
+use solana_program::pubkey::Pubkey;
+use solana_sdk::signature::{Keypair, Signer};
 use solana_test_framework::*;
+use access_protocol::state::Tag;
 
 use crate::common::test_runner::{INITIAL_SUPPLY, TestRunner};
 
@@ -25,51 +27,51 @@ async fn change_inflation() {
     assert_eq!(stats.account.daily_inflation, INITIAL_SUPPLY / 365);
 }
 
-//
-// #[tokio::test]
-// async fn change_authority() {
-//     // Setup the token + basic accounts
-//     let mut tr = TestRunner::new(1_000_000).await.unwrap();
-//     // Change the authority
-//     let new_authority = Keypair::new();
-//     let stats = tr.central_state_stats().await.unwrap();
-//     println!("old authority: {:?}", stats.account.authority);
-//     tr.change_central_state_authority(&new_authority)
-//         .await
-//         .unwrap();
-//     // Check the authority
-//     let stats = tr.central_state_stats().await.unwrap();
-//     assert_eq!(stats.account.authority, new_authority.pubkey());
-// }
-//
-// #[tokio::test]
-// async fn zero_inflation_start() {
-//     // Setup the token + basic accounts
-//     let mut tr = TestRunner::new(0).await.unwrap();
-//     // Sleep for 5 days
-//     tr.sleep(5 * 86400).await.unwrap();
-//     // Create users
-//     let stake_pool_owner = tr.create_user_with_ata().await.unwrap();
-//     // Create a pool
-//     tr.create_stake_pool(&stake_pool_owner.pubkey(), 10000)
-//         .await
-//         .unwrap();
-//     // Check the pool
-//     let stats = tr.pool_stats(stake_pool_owner.pubkey()).await.unwrap();
-//     assert_eq!(
-//         Pubkey::new(&stats.header.owner).to_string(),
-//         stake_pool_owner.pubkey().to_string()
-//     );
-//     assert_eq!(stats.header.tag, Tag::InactiveStakePool as u8);
-//     // Activate stake pool
-//     tr.activate_stake_pool(&stake_pool_owner.pubkey())
-//         .await
-//         .unwrap();
-//     // Crank
-//     tr.crank_pool(&stake_pool_owner.pubkey()).await.unwrap();
-//     // Check the central state
-//     let stats = tr.central_state_stats().await.unwrap();
-//     assert_eq!(stats.account.last_snapshot_offset, 5);
-//     assert_eq!(stats.account.total_staked, 0);
-//     assert_eq!(stats.account.total_staked_snapshot, 0);
-// }
+
+#[tokio::test]
+async fn change_authority() {
+    // Setup the token + basic accounts
+    let mut tr = TestRunner::new(1_000_000).await.unwrap();
+    // Change the authority
+    let new_authority = Keypair::new();
+    let stats = tr.central_state_stats().await.unwrap();
+    println!("old authority: {:?}", stats.account.authority);
+    tr.change_central_state_authority(&new_authority)
+        .await
+        .unwrap();
+    // Check the authority
+    let stats = tr.central_state_stats().await.unwrap();
+    assert_eq!(stats.account.authority, new_authority.pubkey());
+}
+
+#[tokio::test]
+async fn zero_inflation_start() {
+    // Setup the token + basic accounts
+    let mut tr = TestRunner::new(0).await.unwrap();
+    // Sleep for 5 days
+    tr.sleep(5 * 86400).await.unwrap();
+    // Create users
+    let stake_pool_owner = tr.create_user_with_ata().await.unwrap();
+    // Create a pool
+    tr.create_stake_pool(&stake_pool_owner.pubkey(), 10000)
+        .await
+        .unwrap();
+    // Check the pool
+    let stats = tr.pool_stats(stake_pool_owner.pubkey()).await.unwrap();
+    assert_eq!(
+        Pubkey::new(&stats.header.owner).to_string(),
+        stake_pool_owner.pubkey().to_string()
+    );
+    assert_eq!(stats.header.tag, Tag::InactiveStakePool as u8);
+    // Activate stake pool
+    tr.activate_stake_pool(&stake_pool_owner.pubkey())
+        .await
+        .unwrap();
+    // Crank
+    tr.crank_pool(&stake_pool_owner.pubkey()).await.unwrap();
+    // Check the central state
+    let stats = tr.central_state_stats().await.unwrap();
+    assert_eq!(stats.account.last_snapshot_offset, 5);
+    assert_eq!(stats.account.total_staked, 0);
+    assert_eq!(stats.account.total_staked_snapshot, 0);
+}
--- smart-contract/program/tests/freeze_and_renounce.rs
+++ smart-contract/program/tests/freeze_and_renounce.rs
@@ -1,4 +1,4 @@
-use solana_sdk::signature::Signer;
+use solana_sdk::signature::{Keypair, Signer};
 use access_protocol::instruction::ProgramInstruction::AdminProgramFreeze;
 use access_protocol::utils::{get_freeze_mask, get_unfreeze_mask};
 use crate::common::test_runner::TestRunner;
@@ -10,25 +10,44 @@ async fn program_freeze() {
     // Setup the token + basic accounts
     let mut tr = TestRunner::new(1_000_000).await.unwrap();
 
+    // Create a fee authority account
+    let fee_authority = Keypair::new();
+    tr.get_sol(&fee_authority.pubkey(), 100_000_000)
+        .await
+        .unwrap();
+
     // Freeze the program
     let freeze_mask = get_freeze_mask(vec![]);
     println!("freeze mask: {:0128b}", freeze_mask);
     let staker = tr.create_user_with_ata().await.unwrap();
-    tr.freeze_program(freeze_mask).await.unwrap();
+    tr.freeze_program(freeze_mask, None).await.unwrap();
     tr.sleep(1).await.unwrap();
     tr.mint(&staker.pubkey(), 729_999_999_999).await.unwrap_err();
 
     // Unfreeze the program
     let unfreeze_mask = get_unfreeze_mask(vec![]);
     println!("unfreeze mask: {:0128b}", unfreeze_mask);
-    tr.freeze_program(unfreeze_mask).await.unwrap();
+    tr.freeze_program(unfreeze_mask, None).await.unwrap();
     tr.sleep(1).await.unwrap();
     tr.mint(&staker.pubkey(), 729_999_999_999).await.unwrap();
+
+    // Try freezing with different authority
+    tr.freeze_program(freeze_mask, Some(&fee_authority)).await.unwrap_err();
+
+    // Set the authority
+    tr.change_freeze_authority(&fee_authority).await.unwrap();
+    tr.sleep(1).await.unwrap();
+
+    // Partial freeze with the new authority should fail
+    tr.freeze_program(1, Some(&fee_authority)).await.unwrap_err();
+
+    // Full freeze with the new authority should work
+    tr.freeze_program(0, Some(&fee_authority)).await.unwrap();
 
     // Freeze a specific instruction
     let freeze_mask = get_freeze_mask(vec![access_protocol::instruction::ProgramInstruction::AdminMint]);
     println!("freeze mask: {:0128b}", freeze_mask);
-    tr.freeze_program(freeze_mask).await.unwrap();
+    tr.freeze_program(freeze_mask, None).await.unwrap();
     tr.sleep(1).await.unwrap();
     tr.mint(&staker.pubkey(), 729_999_999_999).await.unwrap_err();
     tr.change_inflation(1_100_000_000).await.unwrap();
@@ -36,7 +55,7 @@ async fn program_freeze() {
     // Unfreeze a specific instruction
     let unfreeze_mask = get_unfreeze_mask(vec![access_protocol::instruction::ProgramInstruction::AdminMint]);
     println!("unfreeze mask: {:0128b}", unfreeze_mask);
-    tr.freeze_program(unfreeze_mask).await.unwrap();
+    tr.freeze_program(unfreeze_mask, None).await.unwrap();
     tr.sleep(1).await.unwrap();
     tr.mint(&staker.pubkey(), 729_999_999_999).await.unwrap();
     tr.change_inflation(1_100_000_000).await.unwrap_err();
@@ -44,7 +63,7 @@ async fn program_freeze() {
     // Freeze a specific instruction
     let freeze_mask = get_freeze_mask(vec![access_protocol::instruction::ProgramInstruction::AdminMint]);
     println!("freeze mask: {:0128b}", freeze_mask);
-    tr.freeze_program(freeze_mask).await.unwrap();
+    tr.freeze_program(freeze_mask, None).await.unwrap();
     tr.sleep(1).await.unwrap();
     tr.mint(&staker.pubkey(), 729_999_999_999).await.unwrap_err();
     tr.change_inflation(1_100_000_000).await.unwrap();
@@ -56,5 +75,5 @@ async fn program_freeze() {
     // Unfreeze should not work anymore
     let unfreeze_mask = get_unfreeze_mask(vec![]);
     println!("unfreeze mask: {:0128b}", unfreeze_mask);
-    tr.freeze_program(unfreeze_mask).await.unwrap_err();
+    tr.freeze_program(unfreeze_mask, None).await.unwrap_err();
 }
